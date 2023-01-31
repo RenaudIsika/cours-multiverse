c71940331ec29143c2a0ca2d127fff76ee8cc07f
Renaud <renaudtoyer@gmail.com>
2023-01-31 10:11:46 +0100

    first commit

---
+++ MultiVerse2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiVerse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Master !");
+        }
+    }
+}
